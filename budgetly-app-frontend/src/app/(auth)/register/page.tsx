3c839fa94b46d8df3bed8440cff5e821f41c3bc9
Diego Viana <dsantos@inf.ufsm.br>
2026-02-03 13:08:07 -0300

    Redirect authenticated users from register page

--- budgetly-app-frontend/src/app/(auth)/register/page.tsx
+++ budgetly-app-frontend/src/app/(auth)/register/page.tsx
@@ -1,6 +1,14 @@
+import { getAuthState } from '@/actions/get-auth-state'
 import { Register } from '@/components/auth/register/register'
+import { redirect } from 'next/navigation'
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  const token = await getAuthState()
+
+  if (token) {
+    redirect('/')
+  }
+
   return (
     <div className="flex w-full gap-6">
       <Register />
