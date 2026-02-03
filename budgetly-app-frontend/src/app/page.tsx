f3e085c561c30ef473de385c5f2e187c1d1ebe46
Diego Viana <dsantos@inf.ufsm.br>
2026-02-03 00:45:31 -0300

    Update page.tsx

--- budgetly-app-frontend/src/app/page.tsx
+++ budgetly-app-frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className="flex w-full items-center justify-center bg-gray-400">
       <h1>home page</h1>
     </div>
   )
