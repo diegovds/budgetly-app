7bc5c6056713cd5fa2884c1d4ac4983d95326c33
Diego Viana <dsantos@inf.ufsm.br>
2026-02-03 22:09:39 -0300

    Update my-accounts.tsx

--- budgetly-app-frontend/src/components/home/my-accounts.tsx
+++ budgetly-app-frontend/src/components/home/my-accounts.tsx
@@ -1,4 +1,6 @@
 import { formatCurrency } from '@/utils/format'
+import { CirclePlus } from 'lucide-react'
+import { Button } from '../ui/button'
 import { Account } from './account'
 
 type MyAccountsProps = {
@@ -12,18 +14,21 @@ type MyAccountsProps = {
 
 export function MyAccounts({ accounts, totalBalance }: MyAccountsProps) {
   return (
-    <div className="bg-accent flex-1 rounded p-4">
+    <div className="bg-accent flex-1 space-y-4 rounded p-4">
       <div className="space-y-2">
         <h2 className="text-2xl font-semibold">Minhas Contas</h2>
         <p className="text-muted-foreground text-sm">
           Total: {formatCurrency(totalBalance)}
         </p>
       </div>
-      <div className="mt-4 space-y-6">
+      <div className="space-y-6">
         {accounts.map((account) => (
           <Account key={account.id} account={account} />
         ))}
       </div>
+      <Button variant="outline">
+        <CirclePlus /> Adicionar Nova Conta
+      </Button>
     </div>
   )
 }
