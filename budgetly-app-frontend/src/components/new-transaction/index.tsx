e99bf6d8a6a1fe1aa90bf22b916b5b5c7c7e5bac
Diego Viana <dsantos@inf.ufsm.br>
2026-02-04 17:22:17 -0300

    update NewTransaction component

--- budgetly-app-frontend/src/components/new-transaction/index.tsx
+++ budgetly-app-frontend/src/components/new-transaction/index.tsx
@@ -7,14 +7,40 @@ import {
 } from '@/components/ui/card'
 import { GetAccount200Item, GetCategory200CategoriesItem } from '@/http/api'
 import Link from 'next/link'
+import z from 'zod'
 import { Button } from '../ui/button'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 type NewTransactionProps = {
   accounts: GetAccount200Item[]
   categories: GetCategory200CategoriesItem[]
 }
 
+const createTransactionSchema = z.object({
+  amount: z.number(),
+  description: z.string().nullable(),
+  date: z.iso.datetime(),
+  type: z.enum(['INCOME', 'EXPENSE']),
+  accountId: z.uuid(),
+  categoryId: z.uuid(),
+})
+
+type CreateNewTransactionFormData = z.infer<typeof createTransactionSchema>
+
 export function NewTransaction({ accounts, categories }: NewTransactionProps) {
+  const form = useForm<CreateNewTransactionFormData>({
+    resolver: zodResolver(createTransactionSchema),
+    defaultValues: {
+      type: 'INCOME',
+      amount: 0,
+      date: new Date().toISOString(),
+      accountId: accounts[0]?.id || '',
+      categoryId: categories[0]?.id || '',
+      description: null,
+    },
+  })
+
   return (
     <Card className="h-fit w-full max-w-xl">
       <CardHeader>
