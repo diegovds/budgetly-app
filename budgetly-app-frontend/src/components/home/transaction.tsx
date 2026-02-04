32c69b4c7ef84f693b1bfdad6534379c291f9995
Diego Viana <dsantos@inf.ufsm.br>
2026-02-04 01:24:23 -0300

    Update transaction.tsx

--- budgetly-app-frontend/src/components/home/transaction.tsx
+++ budgetly-app-frontend/src/components/home/transaction.tsx
@@ -1,5 +1,5 @@
 import { GetTransactionsSummary200TransactionsItem } from '@/http/api'
-import { formatCurrency } from '@/utils/format'
+import { formatCurrency, formatDate } from '@/utils/format'
 
 type TransactionProps = {
   transaction: GetTransactionsSummary200TransactionsItem
@@ -10,7 +10,9 @@ export function Transaction({ transaction }: TransactionProps) {
     <div className="ring-muted-foreground flex items-center justify-between rounded p-4 ring">
       <div className="space-y-2">
         <h3 className="text-xl font-medium">{transaction.name}</h3>
-        <p className="text-muted-foreground text-sm">{transaction.date}</p>
+        <p className="text-muted-foreground text-sm">
+          {formatDate(transaction.date)}
+        </p>
       </div>
       <p
         className={`font-semibold ${transaction.amount < 0 ? 'text-red-500' : 'text-green-500'}`}
