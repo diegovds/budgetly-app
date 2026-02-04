4d46439cf2859d3ae483ded1e6d2d39dff944423
Diego Viana <dsantos@inf.ufsm.br>
2026-02-04 14:54:06 -0300

    Create useAccountInsertionMutation.ts

---
+++ budgetly-app-frontend/src/hooks/useAccountInsertionMutation.ts
@@ -0,0 +1,14 @@
+import { postAccount, PostAccount200, PostAccountBody } from '@/http/api'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+
+export function useAccountInsertionMutation() {
+  const router = useRouter()
+
+  return useMutation<PostAccount200, Error, PostAccountBody>({
+    mutationFn: (data) => postAccount(data),
+    onSuccess: () => {
+      router.push('/')
+    },
+  })
+}
