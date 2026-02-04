2e99c7d1ffbfe880214417d99b47c5749e2b9e2c
Diego Viana <dsantos@inf.ufsm.br>
2026-02-04 19:11:10 -0300

    Create useTransactionInsertionMutation.ts

---
+++ budgetly-app-frontend/src/hooks/useTransactionInsertionMutation.ts
@@ -0,0 +1,18 @@
+import {
+  postTransaction,
+  PostTransaction200,
+  PostTransactionBody,
+} from '@/http/api'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+
+export function useTransactionInsertionMutation() {
+  const router = useRouter()
+
+  return useMutation<PostTransaction200, Error, PostTransactionBody>({
+    mutationFn: (data) => postTransaction(data),
+    onSuccess: () => {
+      router.push('/')
+    },
+  })
+}
