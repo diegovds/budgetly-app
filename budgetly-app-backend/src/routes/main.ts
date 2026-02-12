b0313c1c42dcd0ea4fe79b46435e2fc66c867777
Diego Viana <dsantos@inf.ufsm.br>
2026-02-12 17:05:24 -0300

    fix for vercel

--- budgetly-app-backend/src/routes/main.ts
+++ budgetly-app-backend/src/routes/main.ts
@@ -35,4 +35,6 @@ export async function routes(app: FastifyInstance) {
   app.register(getAccounts)
   app.register(listCategories)
   app.register(listTransactionsSummary)
+  app.register(getAccountTypes)
+  app.register(getCategoryTypes)
 }
