d3f6e8ef06d93d07a048e4c2eb436f55df3e3d0a
Diego Viana <dsantos@inf.ufsm.br>
2026-02-24 19:35:32 -0300

    update category page

--- budgetly-app-frontend/src/app/category/page.tsx
+++ budgetly-app-frontend/src/app/category/page.tsx
@@ -1,5 +1,7 @@
 import { getAuthState } from '@/actions/get-auth-state'
 import { HeaderPage } from '@/components/header-page'
+import { getCategory } from '@/http/api'
+import { TrendingDown, TrendingUp } from 'lucide-react'
 import { Metadata } from 'next'
 import { redirect } from 'next/navigation'
 
@@ -14,6 +16,8 @@ export default async function CategoryPage() {
     redirect('/login')
   }
 
+  const { categories, meta } = await getCategory({ limit: 50, page: 1 })
+
   return (
     <div className="w-full space-y-8">
       <HeaderPage
@@ -22,6 +26,24 @@ export default async function CategoryPage() {
         href="/category/new"
         title="Gerenciar Categorias"
       />
+      <div className="flex gap-8">
+        <div className="bg-accent flex-1 space-y-2 rounded p-4">
+          <div className="flex items-center gap-2">
+            <div className="rounded-full bg-green-200 p-1">
+              <TrendingUp className="text-green-500" />
+            </div>
+            <h3 className="font-semibold">Categoria de Receita</h3>
+          </div>
+        </div>
+        <div className="bg-accent flex-1 space-y-2 rounded p-4">
+          <div className="flex items-center gap-2">
+            <div className="rounded-full bg-red-200 p-1">
+              <TrendingDown className="text-red-500" />
+            </div>
+            <h3 className="font-semibold">Categoria de Despesa</h3>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
