232af0d27d7301a0c4dcc7b9ef0a380034514413
Diego Viana <dsantos@inf.ufsm.br>
2026-02-25 15:08:08 -0300

    Update my-transactions.tsx

--- budgetly-app-frontend/src/components/home/my-transactions.tsx
+++ budgetly-app-frontend/src/components/home/my-transactions.tsx
@@ -19,18 +19,24 @@ export function MyTransactions({
     <div className="bg-accent flex-2 space-y-8 rounded p-4 pb-6">
       <div className="space-y-6">
         <div className="flex items-center justify-between">
-          <h2 className="text-2xl font-semibold">Grupos de Transações</h2>
-          <Link href="/category">Ver Tudo</Link>
+          <h2 className="text-xl font-semibold md:text-2xl">
+            Grupos de Transações
+          </h2>
+          <Link href="/category" className="text-xs md:text-sm">
+            Ver Tudo
+          </Link>
         </div>
-        <div className="grid gap-8 lg:grid-cols-2">
+        <div className="grid gap-4 lg:grid-cols-2">
           {categories.map((category) => (
             <Category key={category.id} category={category} />
           ))}
         </div>
       </div>
       <div className="space-y-6">
-        <h2 className="text-2xl font-semibold">Transações Recentes</h2>
-        <div className="grid grid-cols-1 gap-8">
+        <h2 className="text-xl font-semibold md:text-2xl">
+          Transações Recentes
+        </h2>
+        <div className="grid grid-cols-1 gap-4">
           {transactions.map((transaction) => (
             <Transaction key={transaction.id} transaction={transaction} />
           ))}
