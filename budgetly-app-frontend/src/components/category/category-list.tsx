9d15567fb81f3d513afac3d0afbe753cb5799003
Diego Viana <dsantos@inf.ufsm.br>
2026-02-24 21:31:34 -0300

    Update category-list.tsx

--- budgetly-app-frontend/src/components/category/category-list.tsx
+++ budgetly-app-frontend/src/components/category/category-list.tsx
@@ -1,21 +1,67 @@
-import { GetCategory200 } from '@/http/api'
+'use client'
+
+import {
+  getCategory,
+  GetCategory200,
+  GetCategory200CategoriesItemType,
+} from '@/http/api'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { Button } from '../ui/button'
 
 type CategoryListProps = {
   categories: GetCategory200
+  type: GetCategory200CategoriesItemType
 }
 
-export function CategoryList({ categories }: CategoryListProps) {
+export function CategoryList({ categories, type }: CategoryListProps) {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery<GetCategory200>({
+      queryKey: ['categories', type],
+      initialPageParam: 1,
+      queryFn: ({ pageParam }) =>
+        getCategory({
+          type,
+          limit: 9,
+          page: pageParam as number,
+        }),
+      getNextPageParam: (lastPage) => {
+        const { page, totalPages } = lastPage.meta
+
+        if (page < totalPages) {
+          return page + 1
+        }
+
+        return undefined
+      },
+      initialData: {
+        pages: [categories],
+        pageParams: [1],
+      },
+    })
+
   return (
-    <div className="grid gap-8 lg:grid-cols-3">
-      {categories.categories.map((category) => (
-        <div key={category.id}>
-          <div>
-            <h4 className="bg-background rounded p-4 text-center text-sm font-semibold">
-              {category.name}
-            </h4>
-          </div>
-        </div>
-      ))}
+    <div className="flex flex-col gap-6">
+      <div className="grid gap-8 lg:grid-cols-3">
+        {data.pages.map((group) =>
+          group.categories.map((category) => (
+            <div key={category.id}>
+              <h4 className="bg-background rounded p-4 text-center text-sm font-semibold">
+                {category.name}
+              </h4>
+            </div>
+          )),
+        )}
+      </div>
+
+      {hasNextPage && (
+        <Button
+          className="w-fit self-center"
+          onClick={() => fetchNextPage()}
+          disabled={isFetchingNextPage}
+        >
+          {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+        </Button>
+      )}
     </div>
   )
 }
