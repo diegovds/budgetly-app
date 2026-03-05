b54d0f3635bf70dc5020bef1e50f7cb0956fa182
Diego Viana <dsantos@inf.ufsm.br>
2026-03-05 14:14:09 -0300

    Create chart-pie-donut-text.tsx

---
+++ budgetly-app-frontend/src/components/dashboard/chart-pie-donut-text.tsx
@@ -0,0 +1,119 @@
+'use client'
+
+import { Label, Pie, PieChart } from 'recharts'
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from '@/components/ui/chart'
+import { GetDashboardGettopexpensecategories200 } from '@/http/api'
+import { TrendingUp } from 'lucide-react'
+
+export const description = 'A donut chart with text'
+
+const chartConfig = {
+  visitors: {
+    label: 'Visitors',
+  },
+  chrome: {
+    label: 'Chrome',
+    color: 'var(--chart-1)',
+  },
+  safari: {
+    label: 'Safari',
+    color: 'var(--chart-2)',
+  },
+  firefox: {
+    label: 'Firefox',
+    color: 'var(--chart-3)',
+  },
+  edge: {
+    label: 'Edge',
+    color: 'var(--chart-4)',
+  },
+  other: {
+    label: 'Other',
+    color: 'var(--chart-5)',
+  },
+} satisfies ChartConfig
+
+type ChartPieDonutTextProps = {
+  chartData: GetDashboardGettopexpensecategories200
+}
+
+export function ChartPieDonutText({ chartData }: ChartPieDonutTextProps) {
+  return (
+    <Card className="flex flex-1 flex-col p-4">
+      <CardHeader className="items-center p-0">
+        <CardTitle>Gastos por categoria</CardTitle>
+      </CardHeader>
+      <CardContent className="flex-1 p-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-62.5"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData.categories}
+              dataKey="percentage"
+              nameKey="category"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {chartData.totalExpenses}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Despesas
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 leading-none font-medium">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="text-muted-foreground leading-none">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
