6e305e143d94b5467f05ccd00613134c0f9726e2
Diego Viana <dsantos@inf.ufsm.br>
2026-03-10 16:27:13 -0300

    Update my-accounts.tsx

--- budgetly-app-frontend/src/components/home/my-accounts.tsx
+++ budgetly-app-frontend/src/components/home/my-accounts.tsx
@@ -3,6 +3,7 @@
 import { useModalStore } from '@/store/useModalStore.ts'
 import { formatCurrency } from '@/utils/format'
 import { CirclePlus } from 'lucide-react'
+import Link from 'next/link'
 import { Button } from '../ui/button'
 import { Account } from './account'
 
@@ -21,7 +22,12 @@ export function MyAccounts({ accounts, totalBalance }: MyAccountsProps) {
   return (
     <div className="bg-accent flex-1 space-y-4 rounded p-4">
       <div className="space-y-2">
-        <h2 className="text-xl font-semibold md:text-2xl">Minhas Contas</h2>
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold md:text-2xl">Minhas Contas</h2>
+          <Link href="/account" className="text-xs md:text-sm">
+            Ver Todas
+          </Link>
+        </div>
         <p className="text-muted-foreground text-xs md:text-sm">
           Total: {formatCurrency(totalBalance)}
         </p>
