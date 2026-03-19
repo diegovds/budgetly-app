0c8f331ea966cca2b0422e8bbaeda04cad3304d2
Diego Viana <dsantos@inf.ufsm.br>
2026-03-19 17:31:38 -0300

    Update useModalStore.ts.ts

--- budgetly-app-frontend/src/store/useModalStore.ts.ts
+++ budgetly-app-frontend/src/store/useModalStore.ts.ts
@@ -1,19 +1,39 @@
+import {
+  GetAccount200AccountsItem,
+  GetCategory200CategoriesItem,
+  GetTransactions200TransactionsItem,
+} from '@/http/api'
 import { create } from 'zustand'
 
 interface ModalState {
   isOpen: boolean
   whoOpened: string
+  element:
+    | GetTransactions200TransactionsItem
+    | GetCategory200CategoriesItem
+    | GetAccount200AccountsItem
+    | null
   setIsOpen: (isOpen: boolean) => void
   setWhoOpened: (whoOpened: string) => void
+  setElement: (
+    element:
+      | GetTransactions200TransactionsItem
+      | GetCategory200CategoriesItem
+      | GetAccount200AccountsItem,
+  ) => void
   toggleIsOpen: () => void
   toggleWhoOpened: () => void
+  toggleElement: () => void
 }
 
 export const useModalStore = create<ModalState>((set) => ({
   isOpen: false,
   whoOpened: '',
+  element: null,
   setIsOpen: (value) => set({ isOpen: value }),
   setWhoOpened: (value) => set({ whoOpened: value }),
+  setElement: (value) => set({ element: value }),
   toggleIsOpen: () => set((state) => ({ isOpen: !state.isOpen })),
   toggleWhoOpened: () => set({ whoOpened: '' }),
+  toggleElement: () => set({ element: null }),
 }))
