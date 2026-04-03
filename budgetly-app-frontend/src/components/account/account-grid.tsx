d8e63cad65c1006e07bd10ece770d83a7f1e6c2c
Diego Viana <dsantos@inf.ufsm.br>
2026-04-03 17:45:58 -0300

    Update account-grid.tsx

--- budgetly-app-frontend/src/components/account/account-grid.tsx
+++ budgetly-app-frontend/src/components/account/account-grid.tsx
@@ -3,7 +3,7 @@
 import { getAccount, GetAccount200 } from '@/http/api'
 import { useModalStore } from '@/store/useModalStore.ts'
 import { formatCurrency } from '@/utils/format'
-import { useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { ChevronLeft, ChevronRight, Ellipsis, Loader2 } from 'lucide-react'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
@@ -19,7 +19,7 @@ export function AccountGrid() {
   >({
     queryKey: ['accounts', page],
     queryFn: () => getAccount({ limit: 4, page }),
-    placeholderData: (prev) => prev,
+    placeholderData: keepPreviousData,
   })
 
   useEffect(() => {
@@ -28,7 +28,70 @@ export function AccountGrid() {
     }
   }, [isError, error])
 
-  if (!data) return null
+  if (!data)
+    return (
+      <div className="bg-card space-y-6 rounded p-4">
+        <div className="grid gap-8 lg:grid-cols-2">
+          {Array.from({ length: 4 }).map((_, i) => (
+            <div
+              key={i}
+              className="bg-accent flex animate-pulse flex-col rounded p-4 text-transparent"
+            >
+              <div className="flex items-center justify-between gap-2">
+                <h2 className="text-lg font-semibold md:text-xl">_</h2>
+                <Ellipsis size={15} className="cursor-pointer" />
+              </div>
+
+              <p className="text-xs text-transparent md:text-sm">_</p>
+
+              <div className="mt-4 flex items-end justify-between">
+                <div className="space-y-0.5">
+                  <h2 className="text-sm font-semibold text-transparent md:text-base">
+                    Saldo atual
+                  </h2>
+                  <p className={`text-2xl font-semibold md:text-3xl`}>_</p>
+                </div>
+                <Link href={`/transaction?accountId=`}>
+                  <Button
+                    variant="outline"
+                    className="invisible text-xs md:text-sm"
+                  >
+                    Ver Transações
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="bg-accent flex animate-pulse flex-col items-center justify-between gap-4 text-transparent lg:flex-row">
+          <p className="text-xs md:text-sm">Mostrando Contas</p>
+          {isPlaceholderData ? (
+            <Button variant="outline" className="invisible text-xs md:text-sm">
+              <Loader2 className="animate-spin" />
+            </Button>
+          ) : (
+            <div className="invisible flex items-center gap-2">
+              <Button
+                variant="outline"
+                className="text-xs md:text-sm"
+                onClick={() => setPage((p) => p - 1)}
+              >
+                <ChevronLeft />
+              </Button>
+
+              <Button
+                variant="outline"
+                className="text-xs md:text-sm"
+                onClick={() => setPage((p) => p + 1)}
+              >
+                <ChevronRight />
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    )
 
   const currentMeta = data.meta
   const start = (currentMeta.page - 1) * currentMeta.limit + 1
